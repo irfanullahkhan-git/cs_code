647e22696db7e0c125b63292253554d9aa153ad6
Irfanullah Khan <irfanullah.khan@jintech.com>
2024-05-21 10:18:12 +0500

    IK

--- OOP/lab_13.cpp
+++ OOP/lab_13.cpp
@@ -20,6 +20,7 @@ class Rectangle: public Shape{
 		}
 		
 		int getArea(){
+			cout<<"Rectangle Area: ";
 			return width * length;
 		}
 };
@@ -35,12 +36,31 @@ class Triangle: public Shape{
 		}
 		
 		int getArea(){
+			cout<<"Triangle Area: ";
 			return (base * perpendicular)/2;
 		}
 };
 
 int main(){
-	Shape * rect = new Rectangle(10, 20);
-	cout<<"Rectangle Area: "<<rect->getArea();
+	Rectangle rect_1(10, 30); // static 
+	cout<<rect_1.getArea()<<endl;
+	
+	Shape * shape_1 = NULL;
+	
+	int choice;
+	cout<<"Please enter 1 for Rectangle and 2 for Triangle\n";
+	cin>>choice;
+	
+	if(choice == 1){
+		shape_1	= new Rectangle(10, 20);
+	}
+	
+	if(choice == 2){
+		shape_1	= new Triangle(20, 40); 
+	}
+	
+	cout<<shape_1->getArea()<<endl; 
+	
 	return 0;
+	
 }
